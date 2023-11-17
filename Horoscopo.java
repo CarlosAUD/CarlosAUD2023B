2bacb5f1b24e3f7a493eaf0a50b51d2a5aad34b1
AnthonyHaro <150816518+AnthonyHaro@users.noreply.github.com>
2023-11-16 23:19:20 -0500

    Update Horoscopo.java

--- Horoscopo.java
+++ Horoscopo.java
@@ -1,7 +1,33 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {		
-		System.out.println("Bienvenido al cielo");
+		Scanner sc =new Scanner(System.in);
+		Scanner vr = new Scanner(System.in);
+		int a = (int) ((Math.random()*12)+1);
+		int b;
+		int j;
+		int c = (int) ((Math.random()*31)+1);
+		System.out.println("El juego consiste en averigurar la fecha secreta: ");
+		b = sc.nextInt();
+		for (i=0;i<=4;i++){
+			if(a==b){
+				System.out.println("Has acertado");
+				break;}
+			else if(i==4){
+				System.out.println("Lo siento has perdido, el numero correcto era: ");
+				break;}
+			else if(a>b){
+				System.out.println("El numero secreto es Mayor que " + b);}
+			else if(a<b){
+				System.out.println("El numero secreto es Menor que " + b);}
+			if(i<=1){
+				System.out.println("Otro intento: ");
+				b = sc.nextInt();}
+			else if(i==2){
+				int x = (int) (a/10);
+				System.out.println("una pista, la primera cifra es: " + x);
+				System
+				break;}
 	}
 
 }
